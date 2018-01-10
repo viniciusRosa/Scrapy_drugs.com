1c80669b43f48bb2bfb612259261afb4d281ffd2
vinicius rosa <vinicius.rosa00@gmail.com>
2018-01-10 19:00:44 -0200

    crawl all links

--- drugs/spiders/drugs_main.py
+++ drugs/spiders/drugs_main.py
@@ -5,13 +5,42 @@ import scrapy
 class DrugsMainSpider(scrapy.Spider):
     name = 'drugs_main'
     allowed_domains = ['drugs.com']
-    start_urls = ['https://www.drugs.com/alpha/a1.html']
+    first_page_url = ('https://www.drugs.com/alpha/a{}.html')
 
-    def parse(self, response):
+    def __init__(self):
+        self.last_page = None
+
+
+    def start_requests(self):
+        request = scrapy.Request(
+            url=DrugsMainSpider.first_page_url.format(1),
+            callback=self.parse_page,
+        )
+        yield request
+
+    def parse_page(self, response):
+        self.last_page = int(response.xpath('/html/body/div[1]/div/div[1]/div[2]/div[1]/div[2]/div[2]/table/tr/td[2]/a/text()')[-1].extract())
+
+        for page in range(1, self.last_page + 1):
+            print(page)
+            request = scrapy.Request(
+                url=DrugsMainSpider.first_page_url.format(page),
+                callback=self.parse_drugs_link,
+            )
+            print(DrugsMainSpider.first_page_url.format(page))
+            yield request
+
+    def parse_drugs_link(self, response):
+       
         results = response.xpath('/html/body/div[1]/div/div[1]/div[2]/div[1]/div[2]/ul/li/a/@href')
 
-        for number, link in enumerate(results, 1):
-            print("{}. {}" .format(number, link.extract()))
+        for link in results:
+            print("{}" .format(link.extract()))
+            print('-----------')
+            print(response.url)
+            print('-----------')
+
+        
 
 
-        #pass
+    
