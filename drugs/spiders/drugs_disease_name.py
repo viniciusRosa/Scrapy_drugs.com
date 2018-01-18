3a332228a678ae26d4e544ca81693c8f21d115ff
vinicius rosa <vinicius.rosa00@gmail.com>
2018-01-18 20:40:43 -0200

    spider of side effects - medicine is works

--- drugs/spiders/drugs_disease_name.py
+++ drugs/spiders/drugs_disease_name.py
@@ -2,41 +2,38 @@
 import scrapy
 import re
 from drugs.items import DrugsSideEffectsItem
-from scrapy.selector import Selector
-from scrapy.http import HtmlResponse
 
-class DrugsMainSpider(scrapy.Spider):
-    name = 'drugs_main'
+
+class DrugsDiseaseNameSpider(scrapy.Spider):
+    name = 'drugs_diseage_name'
     allowed_domains = ['drugs.com']
-    #Reference link, this shows the side effects of medicines that start with letter 'a'
     first_page_url = ('https://www.drugs.com/sfx-a{}.html')
 
     def __init__(self):
         self.last_page = None
 
+    """ get results of first page """
     def start_requests(self):
-        """ get results of first page to scrap"""
         request = scrapy.Request(
-            url=DrugsMainSpider.first_page_url.format(0),
+            url=DrugsDiseaseNameSpider.first_page_url.format(0),
             callback=self.parse_page,
         )
         yield request
 
+    """ get results of all pages """
     def parse_page(self, response):
-        """ get results of all pages that have will be scraped"""
-        #get the last page
         self.last_page = int(response.xpath('/html/body/div[1]/div/div[1]/div[2]/div[1]/div[2]/div[2]/table/tr/td[2]/a/text()')[-1].extract())
-        
-        for page in range(self.last_page):
+
+        for page in range(0, self.last_page):
             request = scrapy.Request(
-                url=DrugsMainSpider.first_page_url.format(page),
+                url=DrugsDiseaseNameSpider.first_page_url.format(page),
                 callback=self.parse_drugs_link,
-                dont_filter = True
             )
             yield request
 
     def parse_drugs_link(self, response):
         results = response.xpath('/html/body/div[1]/div/div[1]/div[2]/div[1]/div[2]/ul/li/a/@href')
+        #results = response.css('.column-list-2 li a::attr(href)').extract()
 
         for drug_link in results:
             url_drug = 'https://www.drugs.com' + drug_link.extract()
@@ -46,21 +43,26 @@ class DrugsMainSpider(scrapy.Spider):
                 callback=self.parse_drugs_detail_description
             )
             yield request
-            
+
+    # def parse_drugs_detail_description(self, response):
+    #     drugs_sfx_item = DrugsSideEffectsItem()
+    #     drug_name = response.css('h1::text').extract_first()
+    #     drugs_sfx = response.css('.contentBox ul:not(.more-resources-list)>li::text').extract()
+
+    #     drugs_sfx_item['drug_name'] = drug_name[:-13]
+    #     drugs_sfx_item['drug_side_effects'] = drugs_sfx
+    #     yield drugs_sfx_item
+
     def parse_drugs_detail_description(self, response):
         drugs_sfx_item = DrugsSideEffectsItem()
         drug_name = response.css('h1::text').extract_first()
-        drugs_sfx_ul_li_p = response.selector.xpath('//div[@class="contentBox"]/ul/li/p')
-        drugs_sfx_ul_li = response.selector.xpath('//div[@class="contentBox"]/ul/li')
-        drugs_sfx_p_sup = response.selector.xpath('//div[@class="contentBox"]/p/sup')
-        
+        drugs_sfx = response.xpath('//div[@class="contentBox"]/ul/li')
+
         drugs_sfx_item['drug_name'] = drug_name[:-13]
 
-        if drugs_sfx_ul_li_p:
+        if drugs_sfx.css('p'):
             drugs_sfx_item['drug_side_effects'] = response.xpath('//div[@class="contentBox"]/ul/li/p/text()').extract()
-        elif drugs_sfx_ul_li:
+        else:
             drugs_sfx_item['drug_side_effects'] = response.xpath('//div[@class="contentBox"]/ul/li/text()').extract()
-        elif drugs_sfx_p_sup:
-            drugs_sfx_item['drug_side_effects'] =response.selector.xpath('//div[@class="contentBox"]/p/sup/../text()').extract()
-            
+        
         yield drugs_sfx_item
